732dec82451e3dbca0c011ff94911632e4664318
sinalpha <hanhdhx65@gmail.com>
2026-09-23 14:54:06 +0900

    캔버스를 띄움.

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,4 +1,8 @@
 # 실습 과제 진행
+from pico2d import *
+
+open_canvas(800, 600)
+
 def move_circle():
     print("Circle is moving")
     pass
@@ -16,3 +20,5 @@ while True:
     move_rectangle()
     move_triangle()
     pass
+
+close_canvas()
