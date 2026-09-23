b0d8f4bdf29040f65e0d2a625da7a3c29cb7037a
sinalpha <hanhdhx65@gmail.com>
2026-09-23 14:44:20 +0900

    프로그램 구조 완성: 원, 사각형, 삼각형 이동

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,3 +1,18 @@
 # 실습 과제 진행
+def move_circle():
+    print("Circle is moving")
+    pass
+
+def move_rectangle():
+    print("Rectangle is moving")
+    pass
+
+def move_triangle():
+    print("Triangle is moving")
+    pass
+
 while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
     pass
